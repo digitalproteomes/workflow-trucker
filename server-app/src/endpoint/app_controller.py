609a64163d61a16f3915bbed679c6116f5a83bc8
Silvana Albert <59642386+silvanaalbert@users.noreply.github.com>
2020-05-20 14:24:28 +0200

    Feature/server.update.samples (#10)
    * Add support for  create, link, edit and remove samples

--- server-app/src/endpoint/app_controller.py
+++ server-app/src/endpoint/app_controller.py
@@ -15,9 +15,11 @@ CORS(app)
 
 # project section
 
+
 @app.route('/test', methods=['GET'])
 def getTestResponse():
     return "test response"
+
 
 @app.route('/project', methods=['GET'])
 def getAllProjects():
@@ -58,6 +60,136 @@ def getPooledSamples():
     parentId = request.args.get('parentId')
     samples = sampleDAO.getPooledSamples(projectId, parentId)
     return jsonify({'samples': samples, 'totalSize': len(samples)})
+
+
+@app.route('/sample/clinical', methods=['POST'])
+def createClinicalSample():
+    data = request.json
+    projectId = data.get('projectId')
+    sampleId = data.get('sampleId')
+    name = data.get('name')
+
+    new_sample = {
+        "sampleId": sampleId,
+        "name": name,
+        "projectId": projectId,
+        "parentSampleId": 0,
+        "protocolId": "1",
+        "protocolName": "clinical_sample"
+    }
+    created_sample = sampleDAO.createSample(new_sample)
+    if(created_sample == 0):
+        return jsonify({'created_sample_id': 'null', 'success': 'false'})
+    else:
+        return jsonify({'created_sample_id': created_sample.sampleId, 'success': 'true'})
+
+
+@app.route('/sample/individual', methods=['POST'])
+def createIndividualSample():
+    data = request.json
+    projectId = data.get('projectId')
+    sampleId = data.get('sampleId')
+    name = data.get('name')
+    parentSampleId = data.get('parentSampleId')
+
+    new_sample = {
+        "sampleId": sampleId,
+        "name": name,
+        "projectId": projectId,
+        "parentSampleId": parentSampleId,
+        "protocolId": "2",
+        "protocolName": "single_preparation"
+    }
+    created_sample = sampleDAO.createSample(new_sample)
+    if(created_sample == 0):
+        return jsonify({'created_sample_id': 'null', 'success': 'false'})
+    else:
+        return jsonify({'created_sample_id': created_sample.sampleId, 'success': 'true'})
+
+
+@app.route('/sample/pooled', methods=['POST'])
+def createPooledSample():
+    data = request.json
+    projectId = data.get('projectId')
+    sampleId = data.get('sampleId')
+    name = data.get('name')
+    childSampleIds = data.get('childSampleIds')
+
+    new_sample = {
+        "sampleId": sampleId,
+        "name": name,
+        "projectId": projectId,
+        "protocolId": "3",
+        "protocolName": "pooling_preparation"
+    }
+
+    created_sample = sampleDAO.createSample(new_sample)
+    failedIds = []
+    if(created_sample != 0):
+        for i in childSampleIds:
+            if(sampleDAO.updateParentSample(i, sampleId) == 0):
+                failedIds.append(i)
+
+    if(created_sample == 0):
+        return jsonify({'created_sample_id': 'null', 'success': 'false', 'failed_child_ids': failedIds})
+    else:
+        return jsonify({'created_sample_id': created_sample.sampleId, 'success': 'true', 'failed_child_ids': failedIds})
+
+
+@app.route('/sample/fractionated', methods=['POST'])
+def createFractionatedSamples():
+    data = request.json
+    projectId = data.get('projectId')
+    parentSampleId = data.get('parentSampleId')
+    fractionatedSamples = data.get('fractionatedSamples')
+
+    if (sampleDAO.getSampleById(parentSampleId)):
+        successIds = []
+        failedIds = []
+        for i in fractionatedSamples:
+            new_sample = {
+                "sampleId": i['sampleId'],
+                "name": i['name'],
+                "projectId": projectId,
+                "parentSampleId": parentSampleId,
+                "protocolId": "4",
+                "protocolName": "fractionation_preparation"
+            }
+            if(sampleDAO.createSample(new_sample) != 0):
+                successIds.append(i['sampleId'])
+            else:
+                failedIds.append(i['sampleId'])
+        return jsonify({'success': 'true', 'success_child_ids': successIds, 'failed_child_ids': failedIds})
+    else:
+        return jsonify({'success': 'false', 'message': "Parrent sample does not exist"})
+
+
+@app.route('/sample/unlink', methods=['PUT'])
+def removeFromPooledSample():
+    data = request.json
+    sampleId = data.get('sampleId')
+    if(sampleDAO.updateParentSample(sampleId, 0) == 0):
+        return jsonify({'unlinked_sample_id': 'null', 'success': 'false'})
+    else:
+        return jsonify({'unlinked_sample_id': sampleId, 'success': 'true'})
+
+
+@app.route('/sample', methods=['PUT'])
+def updateSampleName():
+    data = request.json
+    sampleId = data.get('sampleId')
+    newName = data.get('newName')
+    if(sampleDAO.updateSampleName(sampleId, newName) == 0):
+        return jsonify({'updated_sample_id': 'null', 'success': 'false'})
+    else:
+        return jsonify({'updatedsample_id': sampleId, 'success': 'true'})
+
+
+@app.route('/sample', methods=['DELETE'])
+def deleteSample():
+    sampleId = request.args.get('sampleId')
+    message = sampleDAO.deleteSample(sampleId)
+    return jsonify({'message': message})
 
 
 if __name__ == '__main__':
--- server-app/src/persistence/sample_DAO.py
+++ server-app/src/persistence/sample_DAO.py
@@ -6,23 +6,42 @@ PROTOCOLS = {'1': 'clinical_sample', '2': 'single_preparation',
 
 def createSample(sampleJson):
     new_sample = Sample(**sampleJson)
-    new_sample.commit()
-    new_sample.dump()
 
-    # print("Created: " + new_sample.name)
-    return Sample.find_one({"projectId": 5})
+    existing_sample_id = Sample.find_one(
+        {"sampleId": int(new_sample.sampleId)})
+    existing_sample_name = Sample.find_one({"name": new_sample.name})
+
+    if(existing_sample_id or existing_sample_name):
+        return 0
+    else:
+        new_sample.commit()
+        new_sample.dump()
+        print("Created: " + new_sample.name)
+        return Sample.find_one({"sampleId": int(new_sample.sampleId)})
 
 
 def updateParentSample(sampleId, parentsampleid):
     sample = Sample.find_one({"sampleId": int(sampleId)})
-    if(sample.sampleId):
+    if(sample):
         sample.dump()
         sample.parentSampleId = parentsampleid
         sample.commit()
         updated_sample = Sample.find_one({"sampleId": int(sampleId)})
-        # print (updated_sample.parentSampleId)
+        return 1
     else:
-        print("Sample not found with id: " + sampleId)
+        return 0
+
+
+def updateSampleName(sampleId, newName):
+    sample = Sample.find_one({"sampleId": int(sampleId)})
+    if(sample):
+        sample.dump()
+        sample.name = newName
+        sample.commit()
+        updated_sample = Sample.find_one({"sampleId": int(sampleId)})
+        return 1
+    else:
+        return 0
 
 
 def getAllSamples(projectId):
@@ -62,6 +81,43 @@ def getPooledSamples(projectId, parentId):
     for i in samples:
         result_samples.append(i.dump())
     return result_samples
+
+
+def getSampleById(id):
+    return Sample.find_one({"sampleId": int(id)})
+
+
+def unlinkSamplesByProtocol(parentId, protocolId):
+    samples = Sample.find({"parentSampleId": int(parentId),
+                           "protocolId": protocolId})
+    for i in samples:
+        updateParentSample(i.sampleId, 0)
+
+
+def deleteSample(id):
+    sampleToDelete = Sample.find_one({"sampleId": int(id)})
+
+    protocol = 0
+    if (sampleToDelete):
+        protocol = sampleToDelete.protocolId
+
+# - when clinical sample: automattically unlink fractionated and individual (updating parentId to 0)
+# - when pooled: automattically unlink clinical samples (updating parentId for clinical to 0)
+
+    if(protocol == 3):
+        unlinkSamplesByProtocol(sampleToDelete.sampleId, 1)
+    elif(protocol == 1):
+        unlinkSamplesByProtocol(sampleToDelete.sampleId, 2)
+        unlinkSamplesByProtocol(sampleToDelete.sampleId, 4)
+
+    if(sampleToDelete):
+        deleted_count = sampleToDelete.delete().deleted_count
+        if(deleted_count > 0):
+            return 'Deleted ' + str(deleted_count) + ' samples(s)'
+        else:
+            return 'Could not delete'
+    else:
+        return 'There is no sample with the id: ' + str(id)
 
 
 if __name__ == '__main__':

c42594be0f19129a4b2848cd74bd201e7f53aeca
silalbert <silalbert@ethz.ch>
2020-11-24 21:12:23 +0100

    Set dynamically the global default project id

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './app/App';
 import * as serviceWorker from './serviceWorker';
+import { Constants } from './app/default-data/constants';
 
-ReactDOM.render(
-    // debt - strict mode removed because of the diagrams library https://github.com/projectstorm/react-diagrams/issues/598#issuecomment-635924991
-    // <React.StrictMode>
-    <App />,
-    // </React.StrictMode>,
-    document.getElementById('root'),
-);
+async function initialize() {
+    await Constants.InitAsync();
+}
+
+initialize().then(() => {
+    ReactDOM.render(
+        // debt - strict mode removed because of the diagrams library https://github.com/projectstorm/react-diagrams/issues/598#issuecomment-635924991
+        // <React.StrictMode>
+        <App />,
+        // </React.StrictMode>,
+        document.getElementById('root'),
+    );
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

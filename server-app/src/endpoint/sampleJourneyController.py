c34e5c76c8910abc98cfb8b78db492f53f199588
silalbert <silalbert@ethz.ch>
2020-12-10 14:19:00 +0100

    Create ms ready samples + mock sample journey json

--- server-app/src/endpoint/sampleJourneyController.py
+++ server-app/src/endpoint/sampleJourneyController.py
@@ -2,10 +2,16 @@ from flask import Blueprint
 from flask import Flask, jsonify, request
 from flask_api import status
 from model.sampleJourney import SampleJourney
+import json
 
 from service import sampleJourneyService
 
 sampleJourney_api = Blueprint('sampleJourney_api', __name__)
+
+
+def load_json(datafile):
+    with open(datafile) as f:
+        return json.load(f)
 
 
 @sampleJourney_api.route("/sampleJourney/sample",  methods=['GET'])
@@ -16,3 +22,9 @@ def getSampleJourney():
         return sampleJourney.toJson(), status.HTTP_200_OK
     else:
         return 'Sample with id does not exist.', status.HTTP_404_NOT_FOUND
+
+
+@sampleJourney_api.route("/sampleJourney/mock",  methods=['GET'])
+def getMockSampleJourney():
+    sample_json = load_json("endpoint/sampleJourneyMock.json")
+    return sample_json, status.HTTP_200_OK
